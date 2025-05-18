adf01924a1639bc7cca7779243c4a15004dedb6a
Muhammad Saifullah <saifullahm2005@gmail.com>
2025-05-18 11:06:50 +0500

    added suspencse to checkout page

--- src/app/(frontend)/checkout/page.tsx
+++ src/app/(frontend)/checkout/page.tsx
@@ -1,10 +1,14 @@
 
 import CheckoutForm from '@/components/CheckoutForm'
+import { Suspense } from 'react'
 
 const CheckoutPage = () => {
   return (
     <section className="flex gap-4 w-full justify-between p-2 py-8">
+      <Suspense fallback={<p>Loading</p>}>
+
         <CheckoutForm />
+      </Suspense>
     </section>
   )
 }
